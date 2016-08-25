359dc59b8063fe2ece4d9ea62e7368e8e327e4f2
ed8009 <emitasov@weezlabs.com>
2016-08-26 01:43:19 +0300

    Create a project architecture

--- Friendship-UP/Friendship-UP/NetworkConstants.h
+++ Friendship-UP/Friendship-UP/NetworkConstants.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  NetworkConstants.h
 //  Friendship-UP
 //
 //  Created by ed8009 on 26.08.16.
 //  Copyright © 2016 ed8009. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#ifndef NetworkConstants_h
+#define NetworkConstants_h
 
-@interface ViewController : UIViewController
 
-
-@end
-
+#endif /* NetworkConstants_h */
